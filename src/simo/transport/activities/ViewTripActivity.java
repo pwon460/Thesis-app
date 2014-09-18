6087172a4e4491c1b1403661df2b3f09d89de754
patrick <patrick.c.wong@gmail.com>
2014-09-19 00:52:39 +1000

    added alertdialog confirmation for back button on view trip activity

--- src/simo/transport/activities/ViewTripActivity.java
+++ src/simo/transport/activities/ViewTripActivity.java
@@ -9,10 +9,14 @@ import simo.transport.helpers.DAOBuilder;
 import simo.transport.helpers.Speaker;
 import simo.transport.helpers.TimeBroadcastReceiver;
 import simo.transport.templates.BasicListenerActivity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.ContextThemeWrapper;
 import android.view.View;
 import android.widget.Button;
 
@@ -146,9 +150,33 @@ public class ViewTripActivity extends BasicListenerActivity {
 
 	@Override
 	public void onBackPressed() {
-		Intent intent = new Intent(this, SelectTransportActivity.class);
-		intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-		startActivity(intent);
+		AlertDialog.Builder adb = new AlertDialog.Builder(
+				new ContextThemeWrapper(this, android.R.style.Theme_Holo_Dialog));
+
+		adb.setMessage("Click OK to go back")
+				.setCancelable(false)
+				.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int id) {
+						// if this button is clicked, close
+						// current activity
+						Intent intent = new Intent(((Dialog) dialog)
+								.getContext(), SelectTransportActivity.class);
+						intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+						startActivity(intent);
+					}
+				})
+				.setNegativeButton("Cancel",
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog, int id) {
+								// if this button is clicked, just close
+								// the dialog box and do nothing
+								dialog.cancel();
+							}
+						});
+
+		AlertDialog alertDialog = adb.create();
+		alertDialog.show();
+
 	}
 
 	public Speaker getSpeaker() {
